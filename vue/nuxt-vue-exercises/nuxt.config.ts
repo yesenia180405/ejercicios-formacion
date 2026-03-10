c4fb91c08da1bf7507e1e61198b5487dbe09386c
yesenia180405 <yes.ale1804@gmail.com>
2026-03-10 16:48:08 +0100

    feat: implemetation of a counter and styles with Tailwind

--- vue/nuxt-vue-exercises/nuxt.config.ts
+++ vue/nuxt-vue-exercises/nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2026-03-10',
   future: { compatibilityVersion: 4 },
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
   typescript: { strict: true },
   css: ['~/assets/styles/main.scss'],
 })
