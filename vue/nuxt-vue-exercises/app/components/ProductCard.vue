748fb838c3a2ef57f02dc8d2a37a1f96f8235a7f
yesenia180405 <yes.ale1804@gmail.com>
2026-03-10 17:40:28 +0100

    feat(ProductCard): creating a new componet for Product Card

---
+++ vue/nuxt-vue-exercises/app/components/ProductCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props=defineProps({
+  image:String,
+  title:String,
+  description:String,
+  price:Number
+});
+</script>
+
+<template>
+  <img src="image" alt="title">
+  <h2>{{ title }}</h2>
+  <p>{{ description }}</p>
+  <p>{{ price }}</p>
+  <button>Comprar</button>
+</template>
