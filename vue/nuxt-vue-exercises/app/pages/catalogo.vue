145c29e42619f2b91ff0edea314da816313453e9
yesenia180405 <yes.ale1804@gmail.com>
2026-03-10 22:18:25 +0100

    feat(pages/catalogo):Creating a page Catalogo

---
+++ vue/nuxt-vue-exercises/app/pages/catalogo.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { mockProducts } from '~/data/mock';
+</script>
+<template>
+    <ProductCard
+    v-for="product in mockProducts"
+    :key="product.id"
+    :image="product.image"
+    :title="product.title"
+    :description="product.description"
+    :price="product.price"
+    />
+
+</template>
