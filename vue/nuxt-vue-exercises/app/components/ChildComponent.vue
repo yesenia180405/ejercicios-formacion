e805b34b6a99ff2a040d643c68af1323851618d6
yesenia180405 <yes.ale1804@gmail.com>
2026-03-11 10:16:07 +0100

    feat(ChildComponent|ParentComponent): Creating a new components for the project

---
+++ vue/nuxt-vue-exercises/app/components/ChildComponent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const saludo = defineEmits(["saludar"]);
+
+function saludar() {
+  saludo("saludar", 'Hola desde el componente hijo');
+}
+
+</script>
+
+<template>
+  <button @click="saludar"> <slot/> </button>
+</template>
--- vue/nuxt-vue-exercises/app/components/ProductCard.vue
+++ vue/nuxt-vue-exercises/app/components/ProductCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const props = defineProps({
+const dates = defineProps({
   image: String,
   title: String,
   description: String,
