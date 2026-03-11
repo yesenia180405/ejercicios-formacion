b34fce7bcb44c97e184642e5cf57f6cc67f208fd
yesenia180405 <yes.ale1804@gmail.com>
2026-03-11 10:18:39 +0100

    fix(ParentComponent):

--- vue/nuxt-vue-exercises/app/components/ParentComponent.vue
+++ vue/nuxt-vue-exercises/app/components/ParentComponent.vue
@@ -11,9 +11,9 @@ function saludoHijo(payload: string) {
 </script>
 
 <template>
-  <ChildComponent @saludar="saludoHijo" v-if="mensaje==''" class="bg-green-500 p-2 border border-green-600 ml-16 rounded-lg 
+  <ChildComponent @saludar="saludoHijo" v-if="mensaje==''" class="bg-green-500 p-2 border border-green-600 ml-7 rounded-lg 
   font-semibold text-white hover:bg-green-700"> Saludar </ChildComponent>
-  <ChildComponent @saludar="saludoHijo" v-else class="bg-red-500 p-2 border border-red-600 ml-16 rounded-lg 
+  <ChildComponent @saludar="saludoHijo" v-else class="bg-red-500 p-2 border border-red-600 ml-8 rounded-lg 
   font-semibold text-white hover:bg-red-700"> Despedirse </ChildComponent>
   <p class="m-5">{{ mensaje }}</p>
 
