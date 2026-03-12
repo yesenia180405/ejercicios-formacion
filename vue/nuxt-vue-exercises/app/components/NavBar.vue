457d4c76a0d9dca3869b549d91495e029ab50321
yesenia180405 <yes.ale1804@gmail.com>
2026-03-12 09:16:44 +0100

    fix(mock.ts,NavBar.vue): creating a new interface fot img

--- vue/nuxt-vue-exercises/app/components/NavBar.vue
+++ vue/nuxt-vue-exercises/app/components/NavBar.vue
@@ -1,9 +1,18 @@
+<script setup>
+import { icons } from '~/data/mock';
+</script>
 <template>
   <nav class="bg-blue-500 color-white
  text-white font-bold flex justify-between items-center  items-stretch">
     <NuxtLink to="/home" class=" text-3xl p-5">
       <TheTitle> Ejercicios de Nuxt</TheTitle>
     </NuxtLink>
+
+    <img v-for="icon in icons"
+    :key="icon.id"
+    :src="icon.image"
+    class="h-10 m-5"/>
+
     <div class="flex gap-3 text-lg">
       <NuxtLink to="/" class="text-white font-semibold hover:bg-blue-700 p-5">
       Contador
--- vue/nuxt-vue-exercises/app/data/mock.ts
+++ vue/nuxt-vue-exercises/app/data/mock.ts
@@ -1,32 +1,49 @@
+import alienIcon from "@/assets/icons/alien.svg";
+import apertureIcon from "@/assets/icons/aperture.svg";
 export interface Product {
-    id: number,
-    image: string,
-    title: string,
-    description: string,
-    price: Number
+  id: number;
+  image: string;
+  title: string;
+  description: string;
+  price: Number;
+}
+
+export interface img {
+  id: number;
+  image: string;
 }
 
 export const mockProducts: Product[] = [
-    {
-        id: 1,
-        image: 'https://static.fnac-static.com/multimedia/Images/FR/NR/84/a8/bf/12560516/1540-1/tsp20220504162152/Proof-Standard-Edition-Coffret.jpg',
-        title: 'Proof',
-        description: 'Is a 3-disc anthology album by BTS, summarizing their career with hits like "Dynamite" and "Butter," plus new tracks including "Yet To Come (The Most Beautiful Moment)',
-        price: 79.99
-    },
-    {
-        id: 2,
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPe3GMRTBExbYG-vKgUzVuPKeTk37bXH1_QA&s',
-        title: 'Arirang',
-        description: 'The album centers on the traditional Korean folk song "Arirang," symbolizing the members roots and reflection on their careers',
-        price: 28.99
-    },
-    {
-        id: 3,
-        image: 'https://images.genius.com/e8c0688c96fd1453836c280b54ac36e8.500x500x1.png',
-        title: 'Love Yourselft:Tear',
-        description: 'explores the dark, painful, and agonizing aspects of separation and toxic love',
-        price: 39.99
-    },
-]
+  {
+    id: 1,
+    image:
+      "https://static.fnac-static.com/multimedia/Images/FR/NR/84/a8/bf/12560516/1540-1/tsp20220504162152/Proof-Standard-Edition-Coffret.jpg",
+    title: "Proof",
+    description:
+      'Is a 3-disc anthology album by BTS, summarizing their career with hits like "Dynamite" and "Butter," plus new tracks including "Yet To Come (The Most Beautiful Moment)',
+    price: 79.99,
+  },
+  {
+    id: 2,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPe3GMRTBExbYG-vKgUzVuPKeTk37bXH1_QA&s",
+    title: "Arirang",
+    description:
+      'The album centers on the traditional Korean folk song "Arirang," symbolizing the members roots and reflection on their careers',
+    price: 28.99,
+  },
+  {
+    id: 3,
+    image:
+      "https://images.genius.com/e8c0688c96fd1453836c280b54ac36e8.500x500x1.png",
+    title: "Love Yourselft:Tear",
+    description:
+      "explores the dark, painful, and agonizing aspects of separation and toxic love",
+    price: 39.99,
+  },
+];
 
+export const icons: img[] = [
+  { id: 1, image: alienIcon },
+  { id: 2, image: apertureIcon },
+];
